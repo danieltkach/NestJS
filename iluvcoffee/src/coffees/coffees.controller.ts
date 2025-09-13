17fb86ec4052dd50e1bbf1c5b38f8643c1a05237
Daniel Tkach <danieltkach@gmail.com>
2025-09-13 13:59:08 -0300

    status code and express decorators

--- iluvcoffee/src/coffees/coffees.controller.ts
+++ iluvcoffee/src/coffees/coffees.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 interface CreateCoffeeDto {
   name: string;
@@ -13,12 +24,19 @@ export class CoffeesController {
     return 'This action returns all coffees';
   }
 
+  @Get()
+  findAll2(@Res() response) {
+    // Express.js example using status() and send() methods
+    response.status(200).send('This action returns all coffees');
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action returns #${id} coffee`;
   }
 
   @Post()
+  @HttpCode(HttpStatus.GONE)
   create(@Body() body: CreateCoffeeDto) {
     return body;
     // return `This action creates a coffee`;
