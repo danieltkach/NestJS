9367daf90c9df7bd33b7eb1e543e805afcba3e61
Daniel Tkach <danieltkach@gmail.com>
2025-09-14 21:56:57 -0300

    pagination

---
+++ iluvcoffee/src/common/dto/pagination-query.dto.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Type } from 'class-transformer';
+import { IsOptional, IsPositive } from 'class-validator';
+
+export class PaginationQueryDto {
+  @Type(() => Number)
+  @IsOptional()
+  @IsPositive()
+  limit: number;
+
+  @Type(() => Number)
+  @IsOptional()
+  @IsPositive()
+  offset: number;
+}
